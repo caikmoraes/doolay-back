96c870d8a2a51f308f67c348ddc3dae96aed2f60
Marcello <marcellorcestaro@gmail.com>
2021-11-28 08:19:05 -0300

    tentativa de acerto do serializer

--- saude/serializers.py
+++ saude/serializers.py
@@ -9,6 +9,7 @@ class ListaSintomasSerializer(serializers.ModelSerializer):
 
 
 class EstadoSaudeSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(format="%Y-%m-%d")
     class Meta:
         model = EstadoSaude
         fields = ['id', 'user', 'estado', 'date']
